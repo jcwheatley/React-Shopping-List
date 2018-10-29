6919e1dcdf9e57872cda2ca30b9d2fff5463c24d
James Wheatley <jcwheatley1@gmail.com>
2018-10-29 01:51:11 -0400

    fixed functionality for browsers without redux

--- client/src/store.js
+++ client/src/store.js
@@ -6,9 +6,9 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose
+const store = createStore(rootReducer, intialState, composeEnhancers(
+    applyMiddleware(...middleware)
 ));
 
 export default store;
